5888f250c34d1e33c26373d2e7bec6457dc99d33
frimoldi.eth <francisco.rimoldi@gmail.com>
2023-07-15 15:43:39 +0200

    Delete contracts directory

--- contracts/contracts/interfaces/IERC20Extended.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: BSL-1.1
-pragma solidity 0.8.16;
-
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-interface IERC20Extended is IERC20 {
-	function decimals() external view returns (uint8);
-}
