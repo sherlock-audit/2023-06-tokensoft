190f72bf5e0d8f026c14ddb8336c290a21891a75
frimoldi.eth <francisco.rimoldi@gmail.com>
2023-07-17 10:02:42 +0200

    Delete contracts directory

--- contracts/utils/assertEnv.js
+++
@@ -1,9 +0,0 @@
-const assertEnv = (name) => {
-	const env = process.env[name]
-
-	if (env == undefined) throw new Error(`environmental variable ${name} is undefined`)
-
-	return env
-}
-
-module.exports = { assertEnv };
